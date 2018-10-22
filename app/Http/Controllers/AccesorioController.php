5a7ed79e8a10a6deaf84492db244c9226cb356ff
Jonathan <jonathan@thothnet.com>
2018-10-21 23:30:48 -0500

    set up

---
+++ app/Http/Controllers/AccesorioController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Accesorio;
+use Illuminate\Http\Request;
+
+class AccesorioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Accesorio  $accesorio
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Accesorio $accesorio)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Accesorio  $accesorio
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Accesorio $accesorio)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Accesorio  $accesorio
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Accesorio $accesorio)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Accesorio  $accesorio
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Accesorio $accesorio)
+    {
+        //
+    }
+}
